e59056902a7e06ce0bcf2d1bac3e843f6e72be2e
Phoenix (Nixy) Camacho <44660515+phoenixrising1800@users.noreply.github.com>
2026-07-19 04:18:13 -0400

    Update Mac-deploy-wazuh.sh
    Move out vars for ease of use

--- Mac-deploy-wazuh.sh
+++ Mac-deploy-wazuh.sh
@@ -4,11 +4,15 @@
 #   Author: Nixy
 #   Note: This is for "Apple Silicon" devices, not Intel. (Ref: https://documentation.wazuh.com/current/installation-guide/wazuh-agent/wazuh-agent-package-macos.html)
 
+# Vars
+CUSTOM_MANAGER_IP='<WAZUH_MANAGER_IP>'
+CUSTOM_AGENT_NAME='<YOUR_AGENT_NAME>'
+
 # To deploy the Wazuh agent on your endpoint, edit the WAZUH_MANAGER variable to contain your Wazuh manager IP address or hostname, and run the following commands
 curl -O https://packages.wazuh.com/4.x/macos/wazuh-agent-4.14.6-1.arm64.pkg
 
 # CHANGE THIS VARIABLE!!!
-echo "WAZUH_MANAGER='<WAZUH_MANAGER_IP>' && WAZUH_AGENT_NAME='<YOUR_AGENT_NAME>'" > /tmp/wazuh_envs && sudo installer -pkg wazuh-agent-4.14.6-1.arm64.pkg -target /
+echo "WAZUH_MANAGER=$CUSTOM_MANAGER_IP && WAZUH_AGENT_NAME=$CUSTOM_AGENT_NAME" > /tmp/wazuh_envs && sudo installer -pkg wazuh-agent-4.14.6-1.arm64.pkg -target /
 
 # Start the Wazuh agent to complete the installation process
 launchctl bootstrap system /Library/LaunchDaemons/com.wazuh.agent.plist
