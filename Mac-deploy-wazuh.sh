2cbd00bc41ea9188b38d2778c6c36f11208d44d5
Phoenix (Nixy) Camacho <44660515+phoenixrising1800@users.noreply.github.com>
2026-07-19 05:29:20 -0400

    Update Mac-deploy-wazuh.sh

--- Mac-deploy-wazuh.sh
+++ Mac-deploy-wazuh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-# Mac-deploy-wazuh.sh: Wazuh agent installation + Sysmon deployment for monitoring MacOS endpoints on a home network.
+# Mac-deploy-wazuh.sh: Wazuh agent installation for monitoring MacOS endpoints on a home network.
 #   Prerequisites: Residential network (optionally supported by Tailscale) with an endpoint set-up as a Wazuh/SIEM "Manager"
 #   Author: Nixy
 #   Note: This is for "Apple Silicon" devices, not Intel. (Ref: https://documentation.wazuh.com/current/installation-guide/wazuh-agent/wazuh-agent-package-macos.html)
@@ -8,6 +8,7 @@ set -eu
 # Vars
 CUSTOM_MANAGER_IP='<WAZUH_MANAGER_IP>'
 CUSTOM_AGENT_NAME='<YOUR_AGENT_NAME>'
+CUSTOM_AGENT_VER='4.14.6'
 
 # Quick input check
 case "$CUSTOM_MANAGER_IP" in
@@ -25,13 +26,13 @@ trap 'rm -rf "$tmpdir"' EXIT
 cd "$tmpdir"
 
 # Currently version 4.14.6 as of 7/19/2026 but this can change. Refer to the MacOS installation guide for the latest.
-curl -O https://packages.wazuh.com/4.x/macos/wazuh-agent-4.14.6-1.arm64.pkg
+curl -f -O https://packages.wazuh.com/4.x/macos/wazuh-agent-4.14.6-1.arm64.pkg
 
 # Commit agent settings for agent pkg install
 echo "WAZUH_MANAGER='$CUSTOM_MANAGER_IP' && WAZUH_AGENT_NAME='$CUSTOM_AGENT_NAME'" > /tmp/wazuh_envs && sudo installer -pkg wazuh-agent-4.14.6-1.arm64.pkg -target /
 
 # Clean up temp envs file consumed by Wazuh's installer's postinstall step to scrub the manager IP from disk(?)
-rm -f /tmp/wazuh_envs
+#rm -f /tmp/wazuh_envs
 
 # Start the Wazuh agent to complete the installation process and print success msg
 sudo launchctl bootstrap system /Library/LaunchDaemons/com.wazuh.agent.plist
