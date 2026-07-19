8ba537fb21cea6ba9863473f89df23ff9e82e5c6
Phoenix (Nixy) Camacho <44660515+phoenixrising1800@users.noreply.github.com>
2026-07-19 03:48:49 -0400

    Create Mac-deploy-wazuh.sh

---
+++ Mac-deploy-wazuh.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Mac-deploy-wazuh.sh: Wazuh agent installation + Sysmon deployment for monitoring MacOS endpoints on a home network.
+#   Prerequisites: Residential network (optionally supported by Tailscale) with an endpoint set-up as a Wazuh/SIEM "Manager"
+#   Author: Nixy
+#   Note: This is for "Apple Silicon" devices, not Intel. (Ref: https://documentation.wazuh.com/current/installation-guide/wazuh-agent/wazuh-agent-package-macos.html)
+
+# To deploy the Wazuh agent on your endpoint, edit the WAZUH_MANAGER variable to contain your Wazuh manager IP address or hostname, and run the following commands
+curl -O https://packages.wazuh.com/4.x/macos/wazuh-agent-4.14.6-1.arm64.pkg
+
+# CHANGE THIS VARIABLE!!!
+echo "WAZUH_MANAGER='<WAZUH_MANAGER_IP>'" > /tmp/wazuh_envs && sudo installer -pkg wazuh-agent-4.14.6-1.arm64.pkg -target /
+
+# Start the Wazuh agent to complete the installation process
+launchctl bootstrap system /Library/LaunchDaemons/com.wazuh.agent.plist
